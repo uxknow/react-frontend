fdd55b0aac2de29387a77f54f17946535ba7ddb9
uxknow <secretnerowww@gmail.com>
2023-01-26 19:46:41 +0200

    Create area chart component - Create graph component - Add option chart - Add chart data

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,8 @@
-import { Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { useMemo } from "react";
-import { useEffect } from "react";
+import { Grid } from "@mui/material";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getFavoriteAssets } from "../../../store/thunks/assets";
+import AreaChart from "../../charts/area-chart/AreaChart";
 import {
   StyledGridCardItem,
   StyledBoxContainer,
@@ -18,9 +17,21 @@ const Home = () => {
   const dispatch = useDispatch();
 
   const favoriteAssetName = useMemo(() => ["bitcoin", "ethereum"], []);
-  const uniqueAssets = [
-    ...new Set(favoriteAssets.map((item) => JSON.stringify(item))),
-  ].map((item) => JSON.parse(item));
+
+  // const uniqueAssets = [
+  //   ...new Set(favoriteAssets.map((item) => JSON.stringify(item))),
+  // ].map((item) => JSON.parse(item));
+
+  // const uniqueAssets = favoriteAssets.filter(
+  //   (item, idx, arr) => idx === arr.findIndex((elem) => elem.name === item.name)
+  // );
+
+  const uniqueAssets = favoriteAssets.reduce((acc, curr) => {
+    if (!acc.find((elem) => elem.name === curr.name)) {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
 
   useEffect(() => {
     favoriteAssetName.forEach((asset) => dispatch(getFavoriteAssets(asset)));
@@ -29,6 +40,20 @@ const Home = () => {
   const renderFavoriteBlock = uniqueAssets.map((item) => {
     const currentPrice = item.data.prices[0][1].toFixed(3);
     const currentCap = item.data.market_caps[0][1].toFixed(0);
+    // console.log(
+    //   [
+    //     ...new Set(
+    //       item.data.prices.map(
+    //         (price) =>
+    //           new Date(price[0]).getDate() +
+    //           "/" +
+    //           new Date(price[0]).getMonth() +
+    //           "/" +
+    //           new Date(price[0]).getFullYear()
+    //       )
+    //     ),
+    //   ].filter((item, i) => i % 8 === 1)
+    // );
     return (
       <Grid item lg={6} sm={6} xs={12} key={item.name}>
         <StyledGridCardItem container>
@@ -46,7 +71,7 @@ const Home = () => {
             </StyledBoxCardPriceBlock>
           </Grid>
           <Grid item lg={6} sm={6} xs={12}>
-            <Typography>Chart</Typography>
+            <AreaChart dataPrices={item.data.prices} />
           </Grid>
         </StyledGridCardItem>
       </Grid>
